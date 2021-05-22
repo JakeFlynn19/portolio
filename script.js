a9acfe6b1fd5e0b85ef49e43eab651b30b102a4f
Jake Flynn <JakeFlynn@Jakes-MBP-4.attlocal.net>
2021-05-22 07:13:40 -0400

    scroll button

--- script.js
+++ script.js
@@ -1,57 +1,66 @@
 $(document).ready(function () {
   $(window).scroll(function () {
     if (this.scrollY > 20) {
-      $('.navbar').addClass("sticky");
+      $(".navbar").addClass("sticky");
     } else {
-      $('.navbar').removeClass("sticky");
+      $(".navbar").removeClass("sticky");
+    }
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
     }
   });
 
+  //   slide up script
 
-//   toggle menu/navbar
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+  });
 
-  $('.menu-btn').click(function () {
-    $('.navbar .menu').toggleClass("active");
-    $('.menu-btn i').toggleClass("active");
+  //   toggle menu/navbar
+
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
   });
 });
 
 // carousel
 
-$('.carousel').owlCarousel({
-    margin: 20,
-    loop: true,
-    autoplayTimeOut: 2000,
-    autoplayHoverPause: true,
-    responsive: {
-        0:{
-            items: 1,
-            nav: false,
-        },
-        600:{
-            items: 2,
-            nav: false,
-        },
-        1000:{
-            items: 3,
-            nav: false,
-        }
-    }
+$(".carousel").owlCarousel({
+  margin: 20,
+  loop: true,
+  autoplayTimeOut: 2000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: false,
+    },
+    600: {
+      items: 2,
+      nav: false,
+    },
+    1000: {
+      items: 3,
+      nav: false,
+    },
+  },
 });
 
-
-// typing animation 
+// typing animation
 
 var typed = new Typed(".typing", {
-    strings: ["Software Engineer.", "Web Developer.", "Programmer."],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
+  strings: ["Software Engineer.", "Web Developer.", "Programmer."],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
 });
 
 var typed = new Typed(".typing-2", {
-    strings: ["Software Engineer.", "Web Developer.", "Programmer."],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
+  strings: ["Software Engineer.", "Web Developer.", "Programmer."],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
 });
